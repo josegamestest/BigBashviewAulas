317e02ff4ebcb509e716ae1c684bf88acad80de7
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2022-09-26 14:21:50 -0300

    Create imagem_html.sh

---
+++ imagem_html.sh
@@ -0,0 +1,29 @@
+#Criado por: joseanastacio (josegamestest (www.josegamestest.com.br))
+#Data:2020/09/26
+
+#!/bin/bash
+echo "
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Imagem com html e css</title>
+        <style>
+            h1{
+                background-color: #111;
+                color: #aaa;
+            }
+            h2{
+                background-color: #11a;
+                color: #aaa;
+            }
+        </style>
+    <head>
+        <body>
+            <h1>Imagens de jogo</h2>
+            <div class="item active">
+                <h2>Esta e uma imagem do minetest</h2>
+                <img src="1.png" alt="imagem1">
+            </div>
+        </body>
+</html>
+"
