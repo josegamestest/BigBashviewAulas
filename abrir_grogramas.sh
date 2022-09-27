e37f2d511a5367e2742322b66215955792302278
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2022-09-27 17:25:06 -0300

    Create abrir_grogramas.sh

---
+++ abrir_grogramas.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "<title>Pagina1</title>
+<font size=4% color=green>
+<h1>Pagina1</h1>
+<h2>Comandos test abrir programas</h2>
+<h3>Links para aplicativos do sistema</h3>
+<a onclick=\"_run('./executar.run')\"> ABRIR higan </a><br>
+<a onclick=\"_run('./gimp.run')\"> ABRIR Gimp </a><br>
+"
