f6f6a1a6ad778798c3c54e548b35964eaad182cf
Jose Anastacio <76887321+josegamestest@users.noreply.github.com>
2022-09-27 17:51:08 -0300

    Create abrir_sequencia_de_programas.sh

---
+++ abrir_sequencia_de_programas.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "<title>Pagina1</title>
+<font size=4% color=green>
+<h1>Pagina1</h1>
+<h2>Comandos test abrir programas</h2>
+<a onclick=\"_run('./executar.run')\"> ABRIR programas </a><br>
+" 
+
+echo "
+Coloque este codigo num arquivo chamado executar.run onde cada linha seja uma programas
+assim que fechar o outro abre altomaticamente
+    nome_programa1
+    nome_programa2
+    nome_programa3
+    nome_programa4
+    nome_programa5
+    nome_programa6
+"
